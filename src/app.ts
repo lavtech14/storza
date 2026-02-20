0a2520ce0569c595b7199df403c97772062d417e
lavtech <lavtech14@gmail.com>
2026-02-20 17:01:03 +0530

    small change

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://storza-client.vercel.app/"],
+    origin: ["http://localhost:5173", "https://storza-client.vercel.app"],
     credentials: true,
   }),
 );
